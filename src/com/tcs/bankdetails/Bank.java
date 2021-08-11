3db9cc34e98e5c30902db3b74fd83ac83dbe5863
unknown <tcsadityasalvi>
2021-08-11 13:09:19 +0000

    Bank Assessment

--- src/com/tcs/bankdetails/Bank.java
+++ src/com/tcs/bankdetails/Bank.java
@@ -7,13 +7,11 @@ public class Bank {
 	private static SavingsAccount savings;
 	private static CurrentAccount current;
 	public static void main(String[] args) {
-		
 		Scanner savcurr = new Scanner(System.in);
 		System.out.println("Account type for a user");
 		String accountType=savcurr.nextLine();
 		if(accountType.equalsIgnoreCase("savings")){
-			
-			 savings= new SavingsAccount(75000L, "Aditya", accountType, new Date(), true);
+			savings= new SavingsAccount(75000L, "Aditya", accountType, new Date(), true);
 		}else if(accountType.equalsIgnoreCase("current")) {
 			 current = new CurrentAccount(60000L, "Aditya", accountType, new Date(), true, 60000L);
 		}
